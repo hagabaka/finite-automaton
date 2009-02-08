7805efffc216c17f0df207f190104e58a5e5bbd1
Yaohan Chen <yaohan.chen@gmail.com>
2009-02-08 06:18:22 -0500

    split FiniteAutomaton::State into finite-automaton/state.rb

--- finite-automaton.rb
+++ finite-automaton.rb
@@ -20,15 +20,7 @@ class FiniteAutomaton
     end
     @accept_states = Set.new
     @alphabet = Set.new
-    @states = Set.new
-    add_state(@start_state)
   end
-
-  def add_state(tag)
-    @states << State.new(tag, self)
-  end
-  alias_method :[], :add_state
-  alias_method :state, :add_state
 
   def add_transition(from, character, to)
     [from, to].each {|state| add_state(state)}
@@ -58,21 +50,3 @@ class FiniteAutomaton
   alias_method :accepts?, :accept?
 end
 
-class FiniteAutomaton::State
-  def initialize(automaton, tag)
-    @automaton = automaton
-    @tag = tag
-  end
-
-  def identifier
-    [@automaton, @tag]
-  end
-
-  def eql?(s)
-    identifier == s.identifier
-  end
-
-  def hash
-    identifier.hash
-  end
-end
---
+++ finite-automaton/state.rb
@@ -0,0 +1,49 @@
+require 'finite-automaton'
+
+class FiniteAutomaton
+  def add_state(tag)
+    @states ||= {}
+    @states[tag] ||= State.new(self, tag)
+  end
+  alias_method :[], :add_state
+  alias_method :state, :add_state
+end
+
+
+class FiniteAutomaton::State
+  attr_reader :tag
+
+  def initialize(automaton, tag)
+    @automaton = automaton
+    @tag = tag
+  end
+
+  def identifier
+    [@automaton, @tag]
+  end
+
+  def eql?(s)
+    identifier == s.identifier
+  end
+
+  def hash
+    identifier.hash
+  end
+
+  def goto_on(target, *characters)
+    characters.each {|c| @automaton.add_transition(tag, c, target.tag)}
+  end
+
+  def start!
+    tap {@automaton.start_state = tag}
+  end
+  alias_method :-@, :start!
+
+  def accept!
+    tap {@automaton.accept_states << tag} 
+  end
+
+  def loop_on(*characters)
+    on(*characters).goto(self)
+  end
+end
